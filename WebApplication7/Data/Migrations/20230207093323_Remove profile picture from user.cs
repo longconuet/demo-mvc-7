cb6ad955dfd4fa6a486401ee00c921ef210cd631
longnt <longnt@omt.vn>
2023-02-07 17:05:56 +0700

    commit

---
+++ WebApplication7/Data/Migrations/20230207093323_Remove profile picture from user.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication7.Data.Migrations
+{
+    public partial class Removeprofilepicturefromuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                schema: "Identity",
+                table: "User");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePicture",
+                schema: "Identity",
+                table: "User",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}
