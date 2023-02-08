530cf0206fc86f3aa4937d4b65d5a30e2fb243ba
longnt <longnt@omt.vn>
2023-02-08 17:46:43 +0700

    commitcommit

---
+++ WebApplication7/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication7.Data;
+using WebApplication7.Requests;
+using WebApplication7.Services;
+using Microsoft.AspNetCore.Identity;
+using WebApplication7.Models;
+using System.Net.Mail;
+
+namespace WebApplication7.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(ApplicationDbContext context, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                var userName = request.Username;
+                if (IsValidEmail(request.Username))
+                {
+                    var user = await _userManager.FindByEmailAsync(request.Username);
+                    if (user != null)
+                    {
+                        userName = user.UserName;
+                    }
+                }
+
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(userName, request.Password, false, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View();
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View();
+        }
+
+        public bool IsValidEmail(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ WebApplication7/Requests/LoginRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication7.Requests
+{
+    public class LoginRequest
+    {
+        [Required]
+        [Display(Name = "Email / Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
