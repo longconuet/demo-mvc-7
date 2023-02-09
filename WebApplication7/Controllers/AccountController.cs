10bd71fa2cf625653f9961e74502e78086ecc363
longnt <longnt@omt.vn>
2023-02-09 08:39:58 +0700

    commit

--- WebApplication7/Controllers/AccountController.cs
+++ WebApplication7/Controllers/AccountController.cs
@@ -7,6 +7,11 @@ using WebApplication7.Services;
 using Microsoft.AspNetCore.Identity;
 using WebApplication7.Models;
 using System.Net.Mail;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text.Encodings.Web;
+using System.Text;
 
 namespace WebApplication7.Controllers
 {
@@ -15,12 +20,19 @@ namespace WebApplication7.Controllers
         private readonly ApplicationDbContext _context;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserStore<ApplicationUser> _userStore;
+        private readonly IUserEmailStore<ApplicationUser> _emailStore;
 
-        public AccountController(ApplicationDbContext context, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        public AccountController(ApplicationDbContext context, 
+            SignInManager<ApplicationUser> signInManager, 
+            UserManager<ApplicationUser> userManager,
+            IUserStore<ApplicationUser> userStore)
         {
             _context = context;
             _signInManager = signInManager;
             _userManager = userManager;
+            _userStore = userStore;
+            _emailStore = GetEmailStore();
         }
 
         public IActionResult Login()
@@ -75,9 +87,65 @@ namespace WebApplication7.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Register()
+        public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                MailAddress address = new MailAddress(request.Email);
+                string userName = address.User;
+                var user = new ApplicationUser
+                {
+                    UserName = userName,
+                    Email = request.Email,
+                    FirstName = request.FirstName,
+                    LastName = request.LastName
+                };
+
+                await _userStore.SetUserNameAsync(user, request.Email, CancellationToken.None);
+                await _emailStore.SetEmailAsync(user, request.Email, CancellationToken.None);
+                var result = await _userManager.CreateAsync(user, request.Password);
+
+                if (result.Succeeded)
+                {
+                    // confirm email
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    await _userManager.ConfirmEmailAsync(user, code);
+
+                    // login
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View();
+        }
+
+        private IUserEmailStore<ApplicationUser> GetEmailStore()
+        {
+            if (!_userManager.SupportsUserEmail)
+            {
+                throw new NotSupportedException("The default UI requires a user store with email support.");
+            }
+            return (IUserEmailStore<ApplicationUser>)_userStore;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
         }
     }
 }
